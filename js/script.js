895873f47cdc3bfed34fbeec81ce3ecd07553ec6
tomito26 <tommybwah@gmail.com>
2021-01-25 12:16:19 +0300

    created a new project

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const quizes = [
+    {
+        quiz:"Who is the founder of python programming language?",
+        a:"Guido Van Rossum",
+        b:"James Gosling",
+        c:"Dennis Ritchie",
+        d:"Ken Thomson",
+        correct:"a"
+    },{
+        quiz:"React javascript framework was founded by which organisation?",
+        a:"Google",
+        b:"Facebook",
+        c:"Twitter",
+        d:"Microsoft",
+        correct:"b"
+    },{
+        quiz:"What is HTML in full?",
+        a:"Higher Transformative Machine Language",
+        b:"HyperText Markup Language",
+        c:"Javascript Notation",
+        d:
+    }
+]
